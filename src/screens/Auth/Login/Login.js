da6a55864c396ef56a961bc78108ff148c1518e8
PedroPortow <pedrolportow@gmail.com>
2024-04-09 17:21:47 -0300

    fix: login input field autocaptalize

--- src/screens/Auth/Login/Login.js
+++ src/screens/Auth/Login/Login.js
@@ -1,51 +1,42 @@
-import React, { useState } from "react";
-import {
-  StyleSheet,
-  View,
-  TextInput,
-  Alert,
-  Image,
-  Pressable,
-} from "react-native";
-import { Text, Button } from "@components";
-import { useUserContext } from "../../../context/UserContext";
+import React, { useState } from 'react'
+import { StyleSheet, View, TextInput, Alert, Image, Pressable } from 'react-native'
+import { Text, Button } from '@components'
+import { useUserContext } from '../../../context/UserContext'
 
 function Login() {
-  const [email, setEmail] = useState("admin@gmail.com");
-  const [password, setPassword] = useState("123456");
-  const { login } = useUserContext();
+  const [email, setEmail] = useState('user@gmail.com')
+  const [password, setPassword] = useState('123456')
+  const { login } = useUserContext()
 
   const handleLogin = async () => {
     try {
-      await login(email, password);
+      await login(email, password)
     } catch (error) {
-      console.error("Erro ao fazer login:", error.message);
+      console.error('Erro ao fazer login:', error.message)
     }
-  };
+  }
 
   return (
     <View style={styles.container}>
-      <Image source={require("@assets/logo.png")} style={styles.logo} />
+      <Image source={require('@assets/logo.png')} style={styles.logo} />
       <View style={styles.inputsContainer}>
         <TextInput
           style={styles.input}
           placeholder="Nome de usuário"
           value={email}
+          autoCapitalize="none"
           onChangeText={(text) => setEmail(text)}
         />
         <TextInput
           style={styles.input}
           placeholder="Senha"
           value={password}
+          autoCapitalize="none"
           onChangeText={(text) => setPassword(text)}
           secureTextEntry
         />
       </View>
-      <Button
-        onPress={handleLogin}
-        theme="primary"
-        style={{ width: "100%", margin: 20 }}
-      >
+      <Button onPress={handleLogin} theme="primary" style={{ width: '100%', margin: 20 }}>
         Entrar
       </Button>
       <Text style={styles.passwordLabel}>Esqueceu a senha?</Text>
@@ -53,44 +44,44 @@ function Login() {
         <Text style={styles.buttonText}>ENTRAR</Text>
       </Pressable> */}
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   background: {
-    backgroundColor: "#479BA7",
+    backgroundColor: '#479BA7',
     flex: 1,
   },
   passwordLabel: {
-    fontWeight: "bold",
-    color: "#AAAAAA",
+    fontWeight: 'bold',
+    color: '#AAAAAA',
     marginTop: 240,
   },
   container: {
-    alignItems: "center",
+    alignItems: 'center',
     paddingHorizontal: 20,
     paddingTop: 150,
-    backgroundColor: "white",
+    backgroundColor: 'white',
     borderRadius: 40,
     marginVertical: 8,
     marginHorizontal: 8,
     flex: 1,
   },
   button: {
-    backgroundColor: "#479BA7",
-    justifyContent: "center",
-    alignItems: "center",
+    backgroundColor: '#479BA7',
+    justifyContent: 'center',
+    alignItems: 'center',
     paddingVertical: 12,
     borderRadius: 50,
     marginTop: 32,
-    alignSelf: "flex-end",
-    flexDirection: "row",
-    width: "100%",
+    alignSelf: 'flex-end',
+    flexDirection: 'row',
+    width: '100%',
     gap: 8,
   },
   buttonText: {
-    color: "white",
-    fontWeight: "700",
+    color: 'white',
+    fontWeight: '700',
     fontSize: 16,
   },
   logo: {
@@ -99,23 +90,23 @@ const styles = StyleSheet.create({
   },
   inputsContainer: {
     gap: 12,
-    width: "100%",
+    width: '100%',
     marginTop: 24,
   },
   title: {
     fontSize: 24,
-    fontWeight: "bold",
+    fontWeight: 'bold',
     marginBottom: 20,
   },
   input: {
-    width: "100%",
+    width: '100%',
     padding: 15,
     borderWidth: 1,
-    color: "black",
-    backgroundColor: "#F6F6F6",
-    borderColor: "#ccc",
+    color: 'black',
+    backgroundColor: '#F6F6F6',
+    borderColor: '#ccc',
     borderRadius: 8,
   },
-});
+})
 
-export default Login;
+export default Login
