697b5666e1ef2bcb386df902dd5b95d527701231
porto <pedrolportow@gmail.com>
2024-05-25 11:41:38 -0300

    using expo url

--- src/services/api.js
+++ src/services/api.js
@@ -6,7 +6,7 @@ import events from '../events'
 const api = axios.create({
   // baseURL: 'http://0.0.0.0:3000/',
   // baseURL: 'http://localhost:3000/',
-  baseURL: 'http://192.168.3.5:3000/',
+  baseURL: EXPO_API_URL,
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
